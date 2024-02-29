1d7900b50f69bf768faf1c8003af477cf8959fea
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2024-02-29 04:43:56 -0900

    Update simple_csv_processing.py

--- super_hero_powers/simple_csv_processing.py
+++ super_hero_powers/simple_csv_processing.py
@@ -75,7 +75,7 @@ st.write(original_data)
 
 # Display the top 10 ability pairs
 st.write("Top 10 Ability Pairs:")
-st.write(top_10_ability_pairs)
+st.write(top_10_ability_pairs, header=["ability_pair", "ability_pair_total"])
 
 # Allow users to download the top 10 ability pairs as a CSV file
 st.markdown("### Download Top 10 Ability Pairs as CSV")
