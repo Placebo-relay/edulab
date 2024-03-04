0741ba9741020fb1d5b6a5c8102c0417127c4ddf
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2024-03-04 05:55:42 -0900

    Update simple_csv_processing.py

--- super_hero_powers/simple_csv_processing.py
+++ super_hero_powers/simple_csv_processing.py
@@ -85,16 +85,14 @@ st.title('CSV Analysis + Jupyter report')
 
 # List of links
 links = {
-    "Streamlit": "https://www.streamlit.io",
+    "🚀Streamlit": "https://www.streamlit.io",
     ":octocat:Jupyter's Github": "https://github.com/jupyter",
     "🌐Jupyter's official site": "https://jupyter.org/",
     "🧪My Google Colab": "https://colab.research.google.com/drive/1KRNDUU2lZpnA1BsovOX7Le8TaZAfNRdp?usp=sharing#scrollTo=4lA0w8FpZMf-",
     "GitHub": "https://github.com",
     "🐍Python": "https://www.python.org",
     "Streamlit": "https://www.streamlit.io"
-}- Google: 
-- Google Colab: 
-- Jupyter: 
+} 
 
 # Display the links in a list format
 st.markdown("### Useful Links:")
