6e83d38b1786669c42aeb33f59787eef22cd3ff5
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2024-03-05 00:36:47 -0900

    Update simple_csv_processing.py

--- super_hero_powers/simple_csv_processing.py
+++ super_hero_powers/simple_csv_processing.py
@@ -93,6 +93,9 @@ links = {
     "🧪🌐My Google Colab": "https://colab.research.google.com/drive/1KRNDUU2lZpnA1BsovOX7Le8TaZAfNRdp?usp=sharing#scrollTo=4lA0w8FpZMf-",
     "🧪🌐Example GC": "https://colab.research.google.com/github/hukenovs/dsp-theory/blob/master/src/dsp_theory_1_signals.ipynb",
     "🔗🌐Example Binder": "https://mybinder.org/repo/AllenDowney/ThinkDSP",
+    "🧑‍💼🌐musthave: LSP ext for jupyter": "https://github.com/jupyter-lsp/jupyterlab-lsp",
+    "🕶️🌐 awesome jupyter ext list": "https://github.com/markusschanta/awesome-jupyter",
+    "🏆🌐 best of jupyter ext list": "https://github.com/ml-tooling/best-of-jupyter",
 } 
 
 # Display the links in a list format
